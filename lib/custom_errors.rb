f5bd2833287a548664e408e503d2747fbcf725ad
stella <sftamesis13@gmail.com>
2023-01-03 14:05:36 -0500

    commit tree 9011622ba9d7845c4c5f6fe9b3a4ca1571e5cc56

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -5,11 +5,25 @@ class Person
     @name = name
   end
 
-  def get_married(person)
-    self.partner = person
-    person.partner = self
+  def get_married(person) 
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      #if the objected passed into this method as an argument is NOT an instance of the person class, we will begin our error handling.
+      begin
+        raise PartnerError #first we raise our Partner error
+      rescue PartnerError => error # then we rescue our PartnerError. The rescue method creates an instance of the PartnerError Class and puts out the result of calling message on that instance.
+        puts error.message
+      end
+    end
   end
 
+  class PartnerError < StandardError
+    def message
+      "you must give the give_married method an argument of an instance of the person class!"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
